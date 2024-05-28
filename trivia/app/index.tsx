5169bfa94cd29174045734f29e565c2d889e2c2e
duonghv1 <thuyduong8122002@gmail.com>
2024-05-27 17:54:22 -0700

    fixed margin in landing page

--- trivia/app/index.tsx
+++ trivia/app/index.tsx
@@ -88,6 +88,7 @@ const styles = StyleSheet.create({
     color: 'white',
     textAlign: 'center',
     marginVertical: 8,
+    marginHorizontal: 10,
   },
   button: {
     backgroundColor: 'red', 
