f0b10c7d98ee6d37c8c019682286898db20e44ad
duonghv1 <thuyduong8122002@gmail.com>
2024-05-29 12:20:00 -0700

    updated answer button styling

--- trivia/components/answerButton.tsx
+++ trivia/components/answerButton.tsx
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
   answerButton: {
     width: "100%",
     height: "auto",
-    minHeight: 95,
+    // minHeight: 95,
     justifyContent: 'center',
     alignItems: 'center',
     marginVertical: 5,
